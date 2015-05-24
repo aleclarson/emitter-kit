04ded433adcc2c416d0fc57804ca925286fb1157
Dominique d'Argent <argent@equinux.com>
2015-05-24 13:46:43 +0200

    Add umbrella header

---
+++ src/EmitterKit.h
@@ -0,0 +1,19 @@
+//
+//  EmitterKit.h
+//  EmitterKit
+//
+//  Created by Dominique d'Argent on 24/05/15.
+//  Copyright (c) 2015 Alec Larson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EmitterKit.
+FOUNDATION_EXPORT double EmitterKitVersionNumber;
+
+//! Project version string for EmitterKit.
+FOUNDATION_EXPORT const unsigned char EmitterKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EmitterKit/PublicHeader.h>
+
+
