aleclarson/emitter-kit

commit a57ccd8f0197be844b57080c65bfd7aecc25a9e9

fix: call order
Previously, a dictionary was used to store listeners, which resulted in indeterminate call order. Using an array ensures the first listener to be created is always the first to be called.

src/EventListener.swift
@@ -10,13 +10,6 @@ public class EventListener <T> : Listener {
     self.event._listeners[_targetID] = listeners
   }
 
-  override func _stopListening () {
-    if self.event == nil { return }
-    var listeners = self.event._listeners[_targetID]!
-    listeners[getHash(self)] = nil
-    self.event._listeners[_targetID] = listeners.nilIfEmpty
-  }
-
   init (_ event: Event<T>, _ target: AnyObject!, _ once: Bool, _ handler: @escaping (T) -> Void) {
     self.event = event
     super.init(target, once, {

tests/EventListenerTests.swift
@@ -18,6 +18,16 @@ class EventListenerTests: XCTestCase {
     calls = 0
   }
 
+  func testCallOrder () {
+    var calls = [Int]()
+    let listeners = [Int](0..<5).map {
+      (i) in event.on { calls.append(i) }
+    }
+
+    event.emit()
+    XCTAssertEqual(calls, [0, 1, 2, 3, 4])
+  }
+
   func testOnce () {
     event.once {
       self.calls += 1

src/Event.swift
@@ -37,16 +37,18 @@ public class Event <T> {
     }
   }
 
-  // 1 - getHash(Listener.target)
-  // 2 - getHash(Listener)
-  // 3 - DynamicPointer<Listener>
-  var _listeners = [String:[String:DynamicPointer<Listener>]]()
+  // key == getHash(listener.target)
+  var _listeners = [String:[DynamicPointer<Listener>]]()
 
   private func _emit (_ data: Any!, on targetID: String) {
-    if let listeners = _listeners[targetID] {
-      for (_, listener) in listeners {
-        listener.object._trigger(data)
-      }
+    if _listeners[targetID] != nil {
+      _listeners[targetID] = _listeners[targetID]!.filter({
+        if let listener = $0.object {
+          listener._trigger(data)
+          return listener._listening
+        }
+        return false
+      }).nilIfEmpty
     }
   }
 

commit 22af7e8c27e3cdec613c515f1e071322cdf593ce

fix: EventListener.isListening = false
Accidentally broken with a57ccd8f0197be844b57080c65bfd7aecc25a9e9

src/EventListener.swift
@@ -14,6 +14,20 @@ public class EventListener <T> : Listener {
     }
   }
 
+  override func _stopListening() {
+    event._listeners[_targetID] =
+      event._listeners[_targetID]!.filter({
+        $0.object !== self
+      }).nilIfEmpty
+  }
+
+  override func _trigger (_ data: Any!) {
+    _handler(data)
+
+    // The listener will be removed by its event.
+    if (self.once) { self._listening = false }
+  }
+
   init (_ event: Event<T>, _ target: AnyObject!, _ once: Bool, _ handler: @escaping (T) -> Void) {
     self.event = event
     super.init(target, once, {

tests/EventListenerTests.swift
@@ -52,6 +52,16 @@ class EventListenerTests: XCTestCase {
     XCTAssertTrue(calls > 1, "EventListener stopped listening after one execution")
   }
   
+  func testOnStopBeforeEmit () {
+    listener = event.on {
+      self.calls += 1
+    }
+    listener.isListening = false
+
+    event.emit()
+    XCTAssertTrue(calls == 0, "'EventListener.isListening = false' has no effect")
+  }
+
   func testOnDeinit () {
     listener = event.on {
       self.calls += 1